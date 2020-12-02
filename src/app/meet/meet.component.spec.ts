6a9f9c19628f2b577da3e1c2a501ad4410343855
James <jhint2011@gmail.com>
2020-12-02 14:53:31 -0500

    update angular

--- src/app/meet/meet.component.spec.ts
+++ src/app/meet/meet.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MeetComponent } from './meet.component';
 
@@ -6,7 +6,7 @@ describe('MeetComponent', () => {
   let component: MeetComponent;
   let fixture: ComponentFixture<MeetComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MeetComponent ]
     })
